83cd1841e9145ef2afbffda0d70c3a5c728c7956
Deep <cdeepkiran14@gmail.com>
2020-06-17 18:19:06 +0530

    Remove dependency on posts and comments service for displaying

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,17 +1,17 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 
-export default({postID})=>{
-    const [comments,setComments]=useState([]);
+export default({comments})=>{
+    /*const [comments,setComments]=useState([]);
 
     const fetchData=async()=>{
         const res=await axios.get(`http://localhost:4001/posts/${postID}/comments`);
 
-    setComments(res.data);
+  //  setComments(res.data);
 };
 
 useEffect(()=>{fetchData();},[]);
-
+*/
 const renderedComments=comments.map(comment=>{
 return <li key={comment.id}>{comment.content}</li>
 });
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -8,8 +8,9 @@ export default () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4000/posts');
-
+   // const res = await axios.get('http://localhost:4000/posts');
+      const res= await axios.get('http://localhost:4002/posts');
+      console.log(res.data);
     setPosts(res.data);
   };
 
@@ -26,7 +27,7 @@ export default () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postID={post.id} />
+          <CommentList comments={post.comments} />
           <CreateComment postID={post.id} />
         
         </div>
