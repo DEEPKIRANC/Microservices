1218f9581d2385e480552379b61d3406e9e6a11c
Deep <cdeepkiran14@gmail.com>
2020-06-19 13:50:56 +0530

    Achieved Event Sync by adding all the emitted events to an event data store

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,9 +5,11 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events=[];
+
 app.post('/events', (req, res) => {
   const event = req.body;
-
+  events.push(event);
   axios.post('http://localhost:4000/events', event);
   axios.post('http://localhost:4001/events', event);
   axios.post('http://localhost:4002/events', event);
@@ -15,6 +17,10 @@ app.post('/events', (req, res) => {
 
   res.send({ status: 'OK' });
 });
+
+app.get('/events',(req,res)=>{
+    res.send(events);
+})
 
 app.listen(4005, () => {
   console.log('Listening on 4005');
